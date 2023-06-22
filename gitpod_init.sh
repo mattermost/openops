f5ee171cf0f080b55024a92bd30f47ed28185656
Christopher Speller <crspeller@gmail.com>
2023-06-22 15:13:35 -0700

    Inital gitpod test.

---
+++ gitpod_init.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+plugin_version="0.1.0"
+
+team_name="main"
+team_display_name="Mattermost AI"
+channel_name="ai"
+channel_display_name="AI"
+user_name="root"
+user_password="$(openssl rand -base64 14)"
+
+echo -e "Setting up Mattermost with ...\n Team name: $team_name\n Team display name: $team_display_name\n Channel name: $channel_name\n Channel display name: $channel_display_name"
+
+echo "Initializing Mattermost for demo. This will take about 30 seconds..."
+
+sleep 35
+
+docker exec mattermost mmctl --local team create --display-name $team_display_name --name $team_name
+docker exec mattermost mmctl --local channel create --team $team_name --display-name "$channel_display_name" --name $channel_name
+
+docker exec mattermost mmctl --local user create --username $user_name --password $user_password --email $user_name@$team_name.com --system-admin --email-verified
+docker exec mattermost mmctl --local team users add $team_name $user_name
+docker exec mattermost mmctl --local channel users add $team_name:$channel_name $user_name
+
+export MM_ADMIN_USERNAME=$user_name
+export MM_ADMIN_PASSWORD=$user_password
+export MM_SERVICESETTINGS_SITEURL=http://localhost:8065
+export MM_SERVICESETTINGS_ENABLEDEVELOPER=true
+
+docker exec mattermost mmctl --local plugins install-url https://github.com/mattermost/mattermost-plugin-ai/releases/download/v$plugin_version/mattermost-ai-$plugin_version.tar.gz
+
+# Configure plugin
+docker exec mattermost bash -c "echo '{\"PluginSettings\":{\"Plugins\":{\"mattermost-ai\":{\"openaicompatibleurl\":\"http://localai:8080\", \"openaicompatiblemodel\":\"ggml-gpt4all-j\",\"llmgenerator\":\"openaicompatible\"}}}}' | mmctl --local config patch /dev/stdin"
+
+echo -e "\n===========================\n\n  THEN LOG IN TO MATTERMOST AT $(gp url 8065)\n\n        username:  $user_name\n        password:  $user_password\n\n  THEN CONFIGURE THE PLUGIN & ENJOY!\n\n"
